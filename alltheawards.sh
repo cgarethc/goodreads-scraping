c2f4612b679da36828ac39b3b7e227d3627b6dba
cgarethc <gareth@cronin.co.nz>
2020-08-02 14:01:52 +1200

    More awards

--- alltheawards.sh
+++ alltheawards.sh
@@ -17,3 +17,13 @@ node populatefirestore.js -a https://www.goodreads.com/award/show/2129-london-bo
 node populatefirestore.js -i pulitzer-fiction -a https://www.goodreads.com/award/show/16-pulitzer-prize -f "Pulitzer Prize for Fiction" -n "Pulitzer Prize for Fiction"
 
 node populatefirestore.js -a https://www.goodreads.com/award/show/21-newbery-medal -f 'Newbery Medal\ \(' -i newbery-winner -n "Newbery Medal"
+
+node populatefirestore.js -a https://www.goodreads.com/award/show/25900-ockham-new-zealand-book-awards -f 'Ockham New Zealand Book Awards for Fiction\ ' -i ockham-fiction -n "Ockham New Zealand Book Awards for Fiction"
+
+node populatefirestore.js -a https://www.goodreads.com/award/show/90-orange-prize -f 'Orange Prize for Fiction\ \(' -i orange-prize -n 'Orange Prize for Fiction'
+
+node populatefirestore.js -a https://www.goodreads.com/award/show/1696-shirley-jackson-award -f 'Shirley Jackson Award for Best Novel\ ' -i shirley-jackson-novel -n "Shirley Jackson Award for Best Novel"
+
+node populatefirestore.js -a https://www.goodreads.com/award/show/111-whitbread-award -f 'Whitbread Award for First Novel' -i whitebread-first-novel -n 'Whitbread Award for First Novel'
+
+node populatefirestore.js -a https://www.goodreads.com/award/show/13720-women-s-prize-for-fiction -f "Women's Prize for Fiction\ \(" -i womens-prize-fiction -n "Women's Prize for Fiction"
