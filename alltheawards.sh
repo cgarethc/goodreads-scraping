58c890cde20c4df7cd98c9debf4ecd85893c1007
cgarethc <gareth@cronin.co.nz>
2020-08-08 12:26:50 +1200

    More lists

--- alltheawards.sh
+++ alltheawards.sh
@@ -27,3 +27,15 @@ node populatefirestore.js -a https://www.goodreads.com/award/show/1696-shirley-j
 node populatefirestore.js -a https://www.goodreads.com/award/show/111-whitbread-award -f 'Whitbread Award for First Novel' -i whitebread-first-novel -n 'Whitbread Award for First Novel'
 
 node populatefirestore.js -a https://www.goodreads.com/award/show/13720-women-s-prize-for-fiction -f "Women's Prize for Fiction\ \(" -i womens-prize-fiction -n "Women's Prize for Fiction"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/6.Best_Books_of_the_20th_Century -p 1 -i best-books-20th -n "Best books of the 20th century"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/4093 -p 1 -i best-books-2010s -n "Best Books of the Decade: 2010s"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/5 -p 1 -i best-books-2000s -n "Best Books of the Decade: 2000s"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/113523.100_Books_about_Science_and_Engineering_for_Grown_Ups -i 100-science-and-engineering -n "100 Books about Science and Engineering for Grown-Ups"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/88513.The_Essential_Computer_Engineer_s_Inspiration -i computer-engineer-inspiration -n "The Essential Computer Engineer's Inspiration"
+
+node populatefirestore.js -l https://www.goodreads.com/list/show/1170.Best_Business_Books -p 2 -i best-business-book -n "The Best Business Books"
