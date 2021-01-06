19549a406b856be53260af4d8cec7c2db6ee296d
cgarethc <gareth@cronin.co.nz>
2021-01-06 14:35:06 +1300

    Start stats doc

--- showmestats.js
+++ showmestats.js
@@ -2,21 +2,24 @@ const cli = require("commander");
 
 const goodreadsShelf = require("./lib/scrapeshelf").scrape;
 const goodreadsBook = require("./lib/scrapebook").scrape;
+const crunchstats = require('./lib/crunchstats');
 
 
 (async () => {
   cli
     .version("0.0.1")
     .arguments("node whatcaniborrow.js")
-    .requiredOption("-u, --user <goodreads user URL>", "User URL")
+    .requiredOption("-u, --user <goodreads user ID>", "User ID")
     .option("-p, --pages <maximum number of pages to scrape>", "Max pages")
+    .option("-l, --limit <maximum number of books to scrape>", "Max books")
     .usage("node index.js [-u userurl]")
     .parse(process.argv);
-  const books = await goodreadsShelf(cli.user, 'read', cli.pages && parseInt(cli.pages));
+  const books = await goodreadsShelf(`https://www.goodreads.com/review/list/${cli.user}`, 'read', cli.pages && parseInt(cli.pages));
   
   console.log('Scraping', books.length, 'books');
 
   const results = [];
+  let counter = 0;
   for (let book of books) {
     process.stdout.write(".");
     const metadata = await goodreadsBook(book.bookURL);
@@ -24,7 +27,17 @@ const goodreadsBook = require("./lib/scrapebook").scrape;
       book,
       metadata
     });
+    counter++;
+    if(cli.limit && counter >= parseInt(cli.limit)){
+      break;
+    }
   }
 
-  console.log('\n', JSON.stringify(results));
+  const genre = await crunchstats.crunchGenre(results);
+  const userStats = {
+    id: cli.user,
+    genre
+  }
+
+  console.log('\n', userStats);
 })();
