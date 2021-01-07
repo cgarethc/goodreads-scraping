b92c175427da3726e14a5f89ae25ef6abdf74e29
cgarethc <gareth@cronin.co.nz>
2021-01-07 15:58:30 +1300

    Group by year read

--- test/crunchstats.test.js
+++ test/crunchstats.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const samplebooks = require('./samplebooks').samplebooks;
+var fs = require('fs');
+var samplebooks = JSON.parse(fs.readFileSync('test/samplebooks.json', 'utf8')).books;
+
 const crunchstats = require('../lib/crunchstats');
 
 test('sanity', () => {
@@ -11,27 +13,98 @@ test('genre', async () => {
   const result = await crunchstats.crunchGenre(samplebooks);
   expect(result['Dystopia'].count).toBe(4);
   expect(result['Dystopia'].books).toEqual(
-    [
-      { "bookTitle": "Severance", "bookAuthor": "Ma, Ling", "bookURL": "https://goodreads.com//book/show/36348525-severance", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1507060524l/36348525._SY75_.jpg", "dateRead": "2020-11-16T00:00:00.000+13:00", "datePub": 2018, "rating": 4 },
-      { "bookTitle": "Eve", "bookAuthor": "Carey, Anna", "bookURL": "https://goodreads.com//book/show/9297774-eve", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389188190l/9297774._SY75_.jpg", "dateRead": "2020-10-07T00:00:00.000+13:00", "datePub": 2011, "rating": 2 },
-      { "bookTitle": "The Testaments", "bookAuthor": "Atwood, Margaret", "bookURL": "https://goodreads.com//book/show/42975172-the-testaments", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1549292344l/42975172._SY75_.jpg", "dateRead": "2019-12-03T00:00:00.000+13:00", "datePub": 2019, "rating": 4 },
-      { "bookTitle": "Machines Like Me", "bookAuthor": "McEwan, Ian", "bookURL": "https://goodreads.com//book/show/42086795-machines-like-me", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552757135l/42086795._SY75_.jpg", "dateRead": "2019-08-06T00:00:00.000+12:00", "datePub": 2019, "rating": 4 }]
+    [{
+      "bookTitle": "Severance",
+      "bookAuthor": "Ma, Ling",
+      "bookURL": "https://goodreads.com//book/show/36348525-severance",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1507060524l/36348525._SY75_.jpg",
+      "dateRead": "2020-11-15T23:00:00.000Z",
+      "yearRead": 2020,
+      "datePub": 2018,
+      "rating": 4
+    },
+    {
+      "bookTitle": "Eve",
+      "bookAuthor": "Carey, Anna",
+      "bookURL": "https://goodreads.com//book/show/9297774-eve",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389188190l/9297774._SY75_.jpg",
+      "dateRead": "2020-10-06T23:00:00.000Z",
+      "yearRead": 2020,
+      "datePub": 2011,
+      "rating": 2
+    },
+    {
+      "bookTitle": "The Testaments",
+      "bookAuthor": "Atwood, Margaret",
+      "bookURL": "https://goodreads.com//book/show/42975172-the-testaments",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1549292344l/42975172._SY75_.jpg",
+      "dateRead": "2019-12-02T23:00:00.000Z",
+      "yearRead": 2019,
+      "datePub": 2019,
+      "rating": 4
+    },
+    {
+      "bookTitle": "Machines Like Me",
+      "bookAuthor": "McEwan, Ian",
+      "bookURL": "https://goodreads.com//book/show/42086795-machines-like-me",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552757135l/42086795._SY75_.jpg",
+      "dateRead": "2019-08-06T00:00:00.000Z",
+      "yearRead": 2019,
+      "datePub": 2019,
+      "rating": 4
+    }]
   );
 
   expect(result['Dystopia'].byPubYear[2019].count).toBe(2);
   expect(result['Dystopia'].byPubYear[2019].books).toEqual(
-    [{ "bookTitle": "The Testaments", "bookAuthor": "Atwood, Margaret", "bookURL": "https://goodreads.com//book/show/42975172-the-testaments", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1549292344l/42975172._SY75_.jpg", "dateRead": "2019-12-03T00:00:00.000+13:00", "datePub": 2019, "rating": 4 },
-    { "bookTitle": "Machines Like Me", "bookAuthor": "McEwan, Ian", "bookURL": "https://goodreads.com//book/show/42086795-machines-like-me", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552757135l/42086795._SY75_.jpg", "dateRead": "2019-08-06T00:00:00.000+12:00", "datePub": 2019, "rating": 4 }]
+    [{
+      "bookTitle": "The Testaments",
+      "bookAuthor": "Atwood, Margaret",
+      "bookURL": "https://goodreads.com//book/show/42975172-the-testaments",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1549292344l/42975172._SY75_.jpg",
+      "dateRead": "2019-12-02T23:00:00.000Z",
+      "yearRead": 2019,
+      "datePub": 2019,
+      "rating": 4
+    },
+    {
+      "bookTitle": "Machines Like Me",
+      "bookAuthor": "McEwan, Ian",
+      "bookURL": "https://goodreads.com//book/show/42086795-machines-like-me",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552757135l/42086795._SY75_.jpg",
+      "dateRead": "2019-08-06T00:00:00.000Z",
+      "yearRead": 2019,
+      "datePub": 2019,
+      "rating": 4
+    }]
   );
 
   expect(result['Dystopia'].byPubYear[2018].count).toBe(1);
   expect(result['Dystopia'].byPubYear[2018].books).toEqual(
-    [{ "bookTitle": "Severance", "bookAuthor": "Ma, Ling", "bookURL": "https://goodreads.com//book/show/36348525-severance", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1507060524l/36348525._SY75_.jpg", "dateRead": "2020-11-16T00:00:00.000+13:00", "datePub": 2018, "rating": 4 }]
+    [{
+      "bookTitle": "Severance",
+      "bookAuthor": "Ma, Ling",
+      "bookURL": "https://goodreads.com//book/show/36348525-severance",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1507060524l/36348525._SY75_.jpg",
+      "dateRead": "2020-11-15T23:00:00.000Z",
+      "yearRead": 2020,
+      "datePub": 2018,
+      "rating": 4
+    }]
   );
 
   expect(result['Dystopia'].byPubYear[2011].count).toBe(1);
   expect(result['Dystopia'].byPubYear[2011].books).toEqual(
-    [{ "bookTitle": "Eve", "bookAuthor": "Carey, Anna", "bookURL": "https://goodreads.com//book/show/9297774-eve", "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389188190l/9297774._SY75_.jpg", "dateRead": "2020-10-07T00:00:00.000+13:00", "datePub": 2011, "rating": 2 }]
+    [{
+      "bookTitle": "Eve",
+      "bookAuthor": "Carey, Anna",
+      "bookURL": "https://goodreads.com//book/show/9297774-eve",
+      "bookCover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389188190l/9297774._SY75_.jpg",
+      "dateRead": "2020-10-06T23:00:00.000Z",
+      "yearRead": 2020,
+      "datePub": 2011,
+      "rating": 2
+    }]
   );
 
 });
