c09af8de6fc032b7af7cfac75244f4976a1095c5
cgarethc <gareth@cronin.co.nz>
2021-03-21 14:46:34 +1300

    Dockerized

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ WORKDIR /usr/src/app
 COPY lib lib
 COPY populatefirestore.js .
 COPY alltheawards.sh .
-COPY oneaward.sh .
 COPY package.json .
+COPY justonelist.json .
 COPY what-can-i-borrow-firebase-adminsdk-8nxq2-60dfb93da5.json .
 RUN npm install
-CMD ["populatefirestore.js", "-a", "https://www.goodreads.com/award/show/2129-london-book-festival", "-i", "london-book-festival", "-n", "London Book Festival",  "-t" ,"Award"]
+ENTRYPOINT [ "node", "populatefirestore.js", "-f", "justonelist.json"]
--- populatefirestore.js
+++ populatefirestore.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const admin = require('firebase-admin');
 const cli = require("commander");
 const auckland = require("./lib/scrapelibrary").search;
@@ -17,11 +18,11 @@ admin.initializeApp({
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
 
-const scrape = async (searchFunction, dbList, titles, dbConsolidatedList) => {
+const scrape = async (searchFunction, listId, listName, listType, dbList, titles, dbConsolidatedList) => {
   const allResults = [];
   let consolidatedResults = [];
 
-  console.log("Searching for", titles.length, "titles at the library");
+  console.log("Searching for", titles.length, "titles at the library", dbList, dbConsolidatedList);
   let titleCounter = 1;
   for (let title of titles) {
     console.log(`Title ${titleCounter++} "${title.bookTitle}" by ${title.bookAuthor}`);
@@ -41,14 +42,17 @@ const scrape = async (searchFunction, dbList, titles, dbConsolidatedList) => {
   }
 
   console.log('Finished searching, adding to database');
+  try {
 
+    let docRef = db.collection(dbList).doc(listId);
+    await docRef.set({ id: listId, name: listName, type: listType, books: allResults });
 
-  let docRef = db.collection(dbList).doc(cli.id);
-  await docRef.set({ id: cli.id, name: cli.name, type: cli.type, books: allResults });
-
-  if (dbConsolidatedList) {
-    docRef = db.collection(dbConsolidatedList).doc(cli.id);
-    await docRef.set({ id: cli.id, name: cli.name, type: cli.type, books: consolidatedResults });
+    if (dbConsolidatedList) {
+      docRef = db.collection(dbConsolidatedList).doc(listId);
+      await docRef.set({ id: listId, name: listName, type: listType, books: consolidatedResults });
+    }
+  } catch (dbError) {
+    console.error('Failed to write to database', dbError);
   }
 
   return allResults;
@@ -58,35 +62,60 @@ const scrape = async (searchFunction, dbList, titles, dbConsolidatedList) => {
   cli
     .version("0.0.1")
     .arguments("node index.js")
+    .option("-f, --inputfile <path to file>", "JSON file containing list, award, filter, id, name, type records")
     .option("-l, --list <goodreads list URL>", "List URL")
     .option("-a, --award <goodreads award URL>", "Award URL")
     .option("-f, --filter <filter for goodreads award type>", "Award filter")
     .option("-p, --pages <maximum number of pages to scrape>", "Max pages")
     .option("-c, --place <Wellington|Auckland>", "Library (defaults to both)")
-    .requiredOption("-i, --id <id for the list>", "ID")
-    .requiredOption("-n, --name <friendly name for the list>", "Name")
-    .requiredOption("-t, --type <friendly name for the type of list>", "Type")
+    .option("-i, --id <id for the list>", "ID")
+    .option("-n, --name <friendly name for the list>", "Name")
+    .option("-t, --type <friendly name for the type of list>", "Type")
     .usage("node index.js [-l listurl]|[-a awardurl] -i <id for list> -n <name for list> -t <type>")
     .parse(process.argv);
-  let titles;
 
-  if (cli.list) {
-    titles = await goodreadsList(cli.list, cli.pages);
-  } else if (cli.award) {
-    titles = await goodreadsAward(cli.award, cli.filter, cli.pages);
-  } else {
-    console.error(cli.helpInformation());
-    process.exit(2);
+  if (cli.inputfile) {
+    const listDefinitions = JSON.parse(fs.readFileSync(cli.inputfile, 'utf8'));
+    for (let definition of listDefinitions.lists) {
+      let titles;
+      console.log('Processing', definition.url, definition.name, definition.id);
+      if (definition.url.includes('/list/')) {
+        titles = await goodreadsList(definition.url, definition.pages);
+      }
+      else if (definition.url.includes('/award/')) {
+        titles = await goodreadsAward(definition.url, definition.filter, definition.pages);
+      }
+      else {
+        console.error('URL not recognised', definition.url);
+      }
+      console.log('Searching Wellington')
+      scrape(wellington, definition.id, definition.name, definition.type, 'welly', titles, 'wellington');
+      console.log('Searching Auckland');
+      scrape(auckland, definition.id, definition.name, definition.type, 'lists', titles, 'auckland');
+    }
+  }
+  else {
+    let titles;
+
+    if (cli.list) {
+      titles = await goodreadsList(cli.list, cli.pages);
+    } else if (cli.award) {
+      titles = await goodreadsAward(cli.award, cli.filter, cli.pages);
+    } else {
+      console.error(cli.helpInformation());
+      process.exit(2);
+    }
+
+
+    if (!cli.place || cli.place === 'Wellington') {
+      console.log('Searching Wellington')
+      scrape(wellington, cli.id, cli.name, cli.type, 'welly', titles, 'wellington');
+    }
+    if (!cli.place || cli.place === 'Auckland') {
+      console.log('Searching Auckland');
+      scrape(auckland, cli.id, cli.name, cli.type, 'lists', titles, 'auckland');
+    }
   }
 
-
-  if (!cli.place || cli.place === 'Wellington') {
-    console.log('Searching Wellington')
-    scrape(wellington, 'welly', titles, 'wellington');
-  }
-  if (!cli.place || cli.place === 'Auckland') {
-    console.log('Searching Auckland');
-    scrape(auckland, 'lists', titles, 'auckland');
-  }
 
 })();
